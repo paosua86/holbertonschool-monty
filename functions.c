fad9c0cadb6d77b975f2125c0d77166822c70c71
paosua86 <paolasuarezb1986@gmail.com>
2022-08-31 17:59:30 -0500

    add file

--- functions.c
+++ functions.c
@@ -1,6 +1,19 @@
 #include "monty.h"
 
 void _push(stack_t **stack, unsigned int line_number);
+{
+	int i;
+	char *arg;
+	
+	arg = strtok(NULL, " \t\r\n\v\f")
+	if (argument == NULL)
+	{
+		dprintf(STDOUT_FILENO, "L%u: usage: push integer\n", argv[1]);
+		/*free*/
+		exit(EXIT_FAILURE);
+	}
+}
+
 
 void _pall(stack_t **stack, unsigned int line_number);
 void _pint(stack_t **stack, unsigned int line_number);
