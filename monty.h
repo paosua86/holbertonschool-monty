d4e7ca338fdcc428fe20401275111a1cc68461f0
paosua86 <paolasuarezb1986@gmail.com>
2022-08-31 11:55:09 -0500

    added struct

--- monty.h
+++ monty.h
@@ -1,5 +1,12 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
