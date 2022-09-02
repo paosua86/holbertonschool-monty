b7d0e2f32787e32812635a3d80f80e96ec0cb2f6
paosua86 <paolasuarezb1986@gmail.com>
2022-09-02 18:07:54 -0500

    added sub

--- functions2.c
+++ functions2.c
@@ -34,3 +34,26 @@ void _nop(stack_t **stack, unsigned int line_number)
 	(void) stack;
 	(void) line_number;
 }
+
+/**
+ * _sub - subtracts the top element of the stack from
+ * the second top element of the stack
+ * @stack: double pointer to header of the stack
+ * @line_number: counter for line number of the file
+ * Return: void
+ */
+
+void _sub(stack_t **stack, unsigned int line_number)
+{
+	stack_t *sub = *stack;
+	size_t len = stack_length(*stack);
+
+	if (len < 2)
+	{
+		dprintf(STDERR_FILENO, "L%d: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	sub = sub->next;
+	sub->n -= (*stack)->n;
+	_pop(stack, line_number);
+}
--- monty_main.c
+++ monty_main.c
@@ -80,7 +80,7 @@ void get_op(stack_t **stack, unsigned int line_number, char *code)
 		{"swap", _swap},
 		{"add", _add},
 		{"nop", _nop},
-		/*{"sub", _sub},*/
+		{"sub", _sub},
 		/*{"div", _div},*/
 		/*{"mul", _mul},*/
 		/*{"mod", _mod},*/
