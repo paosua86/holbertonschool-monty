4cbaa053fd3dd61a3047aae226068ec188a1c8b7
paosua86 <paolasuarezb1986@gmail.com>
2022-09-02 18:48:53 -0500

    change order in pchar

--- functions3.c
+++ functions3.c
@@ -37,15 +37,15 @@ void _mod(stack_t **stack, unsigned int line_number)
 
 void _pchar(stack_t **stack, unsigned int line_number)
 {
-	if ((*stack)->n < 0 || (*stack)->n > 127)
-	{
-		dprintf(STDERR_FILENO, "L%d: can't pchar, value out of range\n", line_number);
-		exit(EXIT_FAILURE);
-	}
     if (*stack == NULL)
 	{
 		dprintf(STDERR_FILENO, "L%d: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	printf("%c\n", (*stack)->n);
+	if ((*stack)->n < 0 || (*stack)->n > 127)
+	{
+		dprintf(STDERR_FILENO, "L%d: can't pchar, value out of range\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+    printf("%c\n", (*stack)->n);
 }
